fd321da3ee7ade8843398902e2887beea6b74e9c
HananFitS <114347723+HananFitS@users.noreply.github.com>
2023-11-23 22:24:49 +0700

    komentar list, listdin, set, map

--- src/ADT/list/list.h
+++ src/ADT/list/list.h
@@ -23,24 +23,55 @@ typedef struct {
 
 /* ********** KONSTRUKTOR ********** */
 ListofPenyanyi MakeList();
+/* I.S. Sembarang */
+/* F.S. Membuat sebuah ListofPenyanyi kosong berkapasitas MaxElList */
+/* Ciri Map kosong : count bernilai Nil */
 
 //* ********** FUNGSI-FUNGSI ********** */
 boolean IsEmptyList(ListofPenyanyi L);
+/* Mengirim true jika List ListofPenyanyi kosong */
+/* Ciri ListofPenanyi kosong : count bernilai Nil */
 
 boolean IsFullList(ListofPenyanyi L);
+/* Mengirim true jika ListofPenyanyi penuh */
+/* Ciri ListofPenyanyi penuh : count bernilai MaxElList */
 
 int BanyakPenyanyi (ListofPenyanyi L);
+/* Mengirimkan nilai banyaknya Penyanyi pada ListofPenyanyi */
+/* I.S. ListofPenyanyi mungkin kosong, dan bisa sudah berisi beberap Penyanyi */
+/* F.S. Mengembalikan banyaknya Penyanyi pada ListofPenyanyi */
 
 boolean isMemberPenyanyi(ListofPenyanyi L, Word X);
+/* Memeriksa apakah sebuah Elmt Penyanyi X terdapat di ListofPenyanyi */
+/* Mengirimkan true jika Elmt Penyanyi X terdapat di ListofPenyanyi */
+/* I.S. Menerima input nama Penyanyi X */
+/* F.S. Mengirimkan true jika nama Penyanyi terdapat dalam ListofPenyanyi
+		Mengirimkan false jika nama Penyanyi tidak terdapat dalam ListofPenyanyi */
 
 int IndeksPenyanyi (ListofPenyanyi L, Word X);
+/* Mengirimkan indeks nilai Elmt Penyanyi X yang ada dalam ListofPenyanyi */
+/* I.S. Menerima input nama Penyanyi X */
+/* F.S. Mengirimkan nilai indeks dari Penyanyi X di ListofPenyanyi
+		Mengirimkan -1 jika Penyanyi tidak terdapat di ListofPenyanyi*/
 
 Word GetPenyanyi(ListofPenyanyi L, Word singer);
+/* Mendapatkan nama album yang sesuai dengan input K di MapofAlbum */
+/* I.S. MapofAlbum sudah terisi */
+/* F.S. Mengirimkan */
 
 void AddPenyanyi (ListofPenyanyi *L, Word singer);
+/* Menambahkan sebuah Elmt Penaynyi singer ke ListofPenyanyi */
+/* I.S. ListofPenyanyi mungkin kosong */
+/* F.S. Menambahkan Elmt Penyanyi singer ke ListofPenyanyi */
 
 void DelPenyanyi (ListofPenyanyi *L, Word singer);
+/* Menghapus sebuah Elmt Penyanyi singer di ListofPenyanyi */
+/* I.S. ListofPenyanyi mungkin kosong */
+/* F.S. Elmt Penyanyi singer di ListofPenyanyi bukan anggota dari ListofPenyanyi */
 
 void DisplayListPenyanyi (ListofPenyanyi L);
+/* Menampilkan daftar Elmt Penyanyi yang ada di ListofPenyanyi */
+/* I.S. ListofPenyanyi mungkin kosong */
+/* F.S. Menampilkan elemen-elemen ListofPenyanyi */
 
 #endif
--- src/ADT/map/map.h
+++ src/ADT/map/map.h
@@ -25,24 +25,55 @@ typedef struct
 
 /* ********** KONSTRUKTOR ********** */
 MapofAlbum CreateEmptyAlbum();
+/* I.S. Sembarang */
+/* F.S. Membuat sebuah MapofAlbum kosong berkapasitas MaxElMap */
+/* Ciri Map kosong : count bernilai Nil */
 
 /* ********** FUNGSI-FUNGSI ********** */
 boolean IsEmptyAlbum(MapofAlbum M);
+/* Mengirim true jika Map MapofAlbum kosong */
+/* Ciri MapofAlbum kosong : count bernilai Nil */
 
 boolean IsFullAlbum(MapofAlbum M);
+/* Mengirim true jika MapofAlbum penuh */
+/* Ciri MapofAlbum penuh : count bernilai MaxElMap */
 
 int BanyakAlbum(MapofAlbum M);
+/* Mengirimkan nilai banyaknya album pada MapofAlbum */
+/* I.S. MapofAlbum mungkin kosong, dan bisa sudah berisi beberap album */
+/* F.S. Mengembalikan banyaknya album pada MapofAlbum */
 
 int IndeksAlbum(MapofAlbum M, Word k);
+/* Mengembalikan atau mengirimkan indeks nilai Elmt album yang ada dalam MapofAlbum */
+/* I.S. Menerima input nama album k */
+/* F.S. Mengirimkan nilai indeks dari album k di MapofAlbum 
+		Mengirimkan -1 jika album tidak terdapat di MapofAlbum*/
 
 boolean IsMemberAlbum(MapofAlbum M, Word k);
+/* Memeriksa apakah sebuah Elmt album k terdapat di MapofAlbum*/
+/* Mengirimkan true jika Elmt album k terdapat di MapofAlbum */
+/* I.S. Menerima input nama album k */
+/* F.S. Mengirimkan true jika nama album terdapat dalam MapofAlbum
+		Mengirimkan false jika nama album tidak terdapat dalam MapofAlbum */
 
 void AddAlbum(MapofAlbum *M, Word k);
+/* Menambahkan sebuah Elmt k ke MapofAlbum */
+/* I.S. MapofAlbum mungkin kosong */
+/* F.S. Menambahkan Elmt k ke MapofAlbum */
 
 void DelAlbum(MapofAlbum *M, Word k);
+/* Menghapus sebuah Elmt album di MapofAlbum */
+/* I.S. MapofAlbum mungkin kosong */
+/* F.S. Elmt k di MapofAlbum bukan anggota dari MapofAlbum */
 
 Sentence GetAlbum(MapofAlbum M, Sentence k);
+/* Mendapatkan nama album yang sesuai dengan input K di MapofAlbum */
+/* I.S. MapofAlbum sudah terisi */
+/* F.S. Mengirimkan */
 
 void DisplayListAlbum (MapofAlbum M);
+/* Menampilkan daftar Elmt album yang ada di MapofAlbum */
+/* I.S. M mungkin kosong */
+/* F.S. Menampilkan nama ListAlbum */
 
 #endif

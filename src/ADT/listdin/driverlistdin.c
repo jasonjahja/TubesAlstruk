98ec016758cb5100507ac77596f5018a7486573a
Afnan <mohafnanfawaz@gmail.com>
2023-11-23 21:10:59 +0700

    fungsi_baru

--- src/ADT/listdin/driverlistdin.c
+++ src/ADT/listdin/driverlistdin.c
@@ -2,60 +2,37 @@
 #include "listdin.h"
 
 int main() {
-    // Create a list of playlists
-    ListofPlaylist playlistCollection = CreateListPlaylist();
+    // Create a playlist
+    ListofPlaylist myPlaylists = CreateListPlaylist();
 
-    // Insert playlists into the collection
-    InsertListPlaylist(&playlistCollection, toKata("Playlist1"));
-    InsertListPlaylist(&playlistCollection, toKata("Playlist2"));
-    printf("^^^^^^^^^^^^^^^^^^^ 1ST STEP ^^^^^^^^^^^^^^^^^^^^^^^\n");
+    // Insert some playlists
+    InsertListPlaylist(&myPlaylists, toKata("Playlist1"));
+    InsertListPlaylist(&myPlaylists, toKata("Playlist2"));
+    InsertListPlaylist(&myPlaylists, toKata("Playlist3"));
 
-    // Display the list of playlists
-    printf("List of Playlists:\n");
-    DisplayListPlaylist(&playlistCollection);
-    printf("^^^^^^^^^^^^^^^^^^^ 2ND STEP ^^^^^^^^^^^^^^^^^^^^^^^\n");
+    // Display the playlists
+    printf("My Playlists:\n");
+    DisplayListPlaylist(&myPlaylists);
 
-    // Check if a playlist is a member of the collection
-    Word searchPlaylist = toKata("Playlist2");
-    if (isMemberListPlaylist(playlistCollection, searchPlaylist)) {
-        printf("this playlist is in the collection : ");
-        TulisWord(searchPlaylist);
-        printf("\n");
+    // Check if a playlist is a member
+    Word playlistToCheck = toKata("Playlist2");
+    if (isMemberListPlaylist(myPlaylists, playlistToCheck)) {
+        TulisWordNoNL(playlistToCheck);
+        printf(" is in the playlist.\n");
     } else {
-        printf("this playlist isn't in the collection : ");
-        TulisWord(searchPlaylist);
-        printf("\n");
+        TulisWordNoNL(playlistToCheck);
+        printf(" is not in the playlist.\n");
     }
 
-    printf("^^^^^^^^^^^^^^^^^^^ 3RD STEP ^^^^^^^^^^^^^^^^^^^^^^^\n");
+    // Delete a playlist
+    DeleteListPlaylist(&myPlaylists, 1);
 
-    // Get the index of a playlist in the collection
-    int index = IndexListPlaylist(playlistCollection, searchPlaylist);
-    if (index >= 0) {
-        TulisWord(searchPlaylist);
-        printf(" is at index %d in the collection.\n", index);
-    } else {
-        TulisWord(searchPlaylist);
-        printf(" %s is not found in the collection.\n");
-    }
+    // Display the playlists after deletion
+    printf("\nAfter Deletion:\n");
+    DisplayListPlaylist(&myPlaylists);
 
-    printf("^^^^^^^^^^^^^^^^^^^ 4TH STEP ^^^^^^^^^^^^^^^^^^^^^^^\n");
-
-    // Delete a playlist from the collection
-    Word deletedPlaylist;
-    deletedPlaylist = toKata("Playlist1");
-    DeleteListPlaylist(&playlistCollection, deletedPlaylist);
-    printf("Deleted Playlist: ");
-    TulisWord(deletedPlaylist);
-    printf("\n");
-
-    printf("^^^^^^^^^^^^^^^^^^^ 5TH STEP ^^^^^^^^^^^^^^^^^^^^^^^\n");
-
-    // Display the updated list of playlists
-    printf("Updated List of Playlists:\n");
-    DisplayListPlaylist(&playlistCollection);
-
-    printf("^^^^^^^^^^^^^^^^^^^ 6TH STEP ^^^^^^^^^^^^^^^^^^^^^^^\n");
+    // Deallocate the playlist
+    DeallocateListPlaylist(&myPlaylists);
 
     return 0;
 }
